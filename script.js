7a4b326f2d940078d9cc2a9686e1209244a5bf50
yesenyn <esenalisher82@gmail.com>
2025-11-05 00:56:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const form = document.getElementById('survey-form');
 const successMsg = document.getElementById('success-msg');
 
 // НОВЫЙ УНИКАЛЬНЫЙ URL Formspree для вашего опроса
-const FORMSPREE_URL = 'https://formspree.io/f/xqagvvqe'; 
+const FORMSPREE_URL = 'https://formspree.io/f/xanlgzge'; 
 
 form.addEventListener('submit', async (e) => {
   // Предотвращаем стандартную отправку формы и перезагрузку страницы
@@ -60,4 +60,5 @@ form.addEventListener('submit', async (e) => {
     }, 5000);
   }
 });
+
 
